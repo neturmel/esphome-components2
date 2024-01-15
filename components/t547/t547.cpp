a471e5ed8d6f948a306e83d725d2e65d09195600
neturmel <neturmel@gmx.de>
2024-01-15 07:53:21 +0100

    Update t547.cpp

--- components/t547/t547.cpp
+++ components/t547/t547.cpp
@@ -1,106 +1,64 @@
-#include "t547.h"
-#include "esphome/core/log.h"
-#include "esphome/core/application.h"
-#include "esphome/core/helpers.h"
-#include "logo.h"
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/core/hal.h"
+#include "esphome/core/version.h"
+#include "esphome/components/display/display_buffer.h"
+
+#include "epd_driver.h"
 
 #ifdef USE_ESP32_FRAMEWORK_ARDUINO
-
-#include <esp32-hal-gpio.h>
 
 namespace esphome {
 namespace t547 {
 
-static const char *const TAG = "t574";
+class T547 : public display::DisplayBuffer {
+public:
+  void set_greyscale(bool greyscale) {
+    this->greyscale_ = greyscale;
+    this->initialize_();
+  }
+  void set_invert(bool invert) { this->invert_ = invert; }
 
-void T547::setup() {
-  this->initialize_();
-  this->clean();
-  this->display();
-}
+  float get_setup_priority() const override;
 
-void T547::initialize_() {
-  ESP_LOGV(TAG, "Initialize called");
-  uint32_t buffer_size = this->get_buffer_length_();
+  void dump_config() override;
 
-  if (this->buffer_ != nullptr) {
-    free(this->buffer_);  // NOLINT
+  void display();
+  void clean();
+  void update() override;
 
-  epd_init();
+  void setup() override;
+
+  uint8_t get_panel_state() { return this->panel_on_; }
+  bool get_greyscale() { return this->greyscale_; }
+
+
+  display::DisplayType get_display_type() override {
+    return get_greyscale() ? display::DisplayType::DISPLAY_TYPE_GRAYSCALE : display::DisplayType::DISPLAY_TYPE_BINARY;
   }
 
-  this->buffer_ = (uint8_t *) ps_malloc(buffer_size);
+ protected:
+  void draw_absolute_pixel_internal(int x, int y, Color color) override;
+  void initialize_();
 
-  if (this->buffer_ == nullptr) {
-    ESP_LOGE(TAG, "Could not allocate buffer for display!");
-    this->mark_failed();
-    return;
-  }
-  uint8_t background = invert_ ? 255 : 0;
-  memset(this->buffer_, background, buffer_size);
-  ESP_LOGV(TAG, "Initialize complete");
-}
+  void eink_off_();
+  void eink_on_();
 
-float T547::get_setup_priority() const { return setup_priority::PROCESSOR; }
-size_t T547::get_buffer_length_() {
-    return this->get_width_internal() * this->get_height_internal() / 2;
-}
 
-void T547::update() {
-  this->do_update_();
-  this->display();
-}
+  int get_width_internal() override { return 960; }
 
-void HOT T547::draw_absolute_pixel_internal(int x, int y, Color color) {
-  if (x >= this->get_width_internal() || y >= this->get_height_internal() || x < 0 || y < 0)
-    return;
-  uint8_t gs = ((color.red * 2126 / 10000) + (color.green * 7152 / 10000) + (color.blue * 722 / 10000));
-  if (invert_) {
-    gs = 255 - gs;
-  }
-  epd_draw_pixel(x, y, gs, this->buffer_);
-}
+  int get_height_internal() override { return 540; }
 
-void T547::dump_config() {
-  LOG_DISPLAY("", "T547", this);
-  LOG_UPDATE_INTERVAL(this);
-}
+  size_t get_buffer_length_();
 
-void T547::eink_off_() {
-  ESP_LOGV(TAG, "Eink off called");
-  if (panel_on_ == 0)
-    return;
-  epd_poweroff();
-  panel_on_ = 0;
-}
+  uint8_t panel_on_ = 0;
+  uint8_t temperature_;
 
-void T547::eink_on_() {
-  ESP_LOGV(TAG, "Eink on called");
-  if (panel_on_ == 1)
-    return;
-  epd_poweron();
-  panel_on_ = 1;
-}
+  bool greyscale_;
+  bool invert_;
+};
 
-void T547::display() {
-  ESP_LOGV(TAG, "Display called");
-  uint32_t start_time = millis();
-
-  epd_poweron();
-  epd_clear();
-  epd_draw_grayscale_image(epd_full_screen(), this->buffer_);
-  epd_poweroff();
-
-  ESP_LOGV(TAG, "Display finished (full) (%ums)", millis() - start_time);
-}
-
-void T547::clean() {
-  ESP_LOGV(TAG, "Clean called");
-  uint32_t start_time = millis();
-  epd_clear();
-
-  ESP_LOGV(TAG, "Clean finished (%ums)", millis() - start_time);
-}
 }  // namespace T547
 }  // namespace esphome
 
